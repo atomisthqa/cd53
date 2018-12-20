e64cec0213d4b44aac08278f00248bf574141415
Christian Dupuis <cd@atomist.com>
2018-12-20 12:46:15 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Cd53Application.java
@@ -0,0 +1,12 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd53Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Cd53Application.class, args);
+	}
+
+}
